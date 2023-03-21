0481bf19d324ec397bc46aa794360de9a3ec7304
Alex <info.alessandrocoraglia@gmail.com>
2023-03-21 16:01:19 +0800

    Lesson 18
    Lesson about lights.
    Created the component Lights the includes the Leva controls for each type of light.
    Added a mesh within the point light to simulate the actual bulb.

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,148 @@ import { Canvas } from '@react-three/fiber'
 import Polyhedron from './Polyhedron'
 import * as THREE from 'three'
 import { Stats, OrbitControls } from '@react-three/drei'
+import { useControls } from 'leva'
+import { useRef } from 'react'
+
+function Lights() {
+  const ambientRef = useRef()
+  const directionalRef = useRef()
+  const pointRef = useRef()
+  const spotRef = useRef()
+
+  useControls('Ambient Light', {
+    visible: {
+      value: false,
+      onChange: (v) => {
+        ambientRef.current.visible = v
+      }
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        ambientRef.current.color = new THREE.Color(v)
+      }
+    }
+  })
+
+  useControls('Directional Light', {
+    visible: {
+      value: true,
+      onChange: (v) => {
+        directionalRef.current.visible = v
+      }
+    },
+    position: {
+      x: 1,
+      y: 1,
+      z: 1,
+      onChange: (v) => {
+        directionalRef.current.position.copy(v)
+      }
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        directionalRef.current.color = new THREE.Color(v)
+      }
+    }
+  })
+
+  useControls('Point Light', {
+    visible: {
+      value: false,
+      onChange: (v) => {
+        pointRef.current.visible = v
+      }
+    },
+    position: {
+      x: 2,
+      y: 0,
+      z: 0,
+      onChange: (v) => {
+        pointRef.current.position.copy(v)
+      }
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        pointRef.current.color = new THREE.Color(v)
+      }
+    }
+  })
+
+  useControls('Spot Light', {
+    visible: {
+      value: false,
+      onChange: (v) => {
+        spotRef.current.visible = v
+      }
+    },
+    position: {
+      x: 3,
+      y: 2.5,
+      z: 1,
+      onChange: (v) => {
+        spotRef.current.position.copy(v)
+      }
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        spotRef.current.color = new THREE.Color(v)
+      }
+    }
+  })
+
+  return (
+    <>
+      <ambientLight ref={ambientRef} />
+      <directionalLight ref={directionalRef} />
+      <pointLight ref={pointRef}>
+        {/* This mesh sphere creates a little ball that simulates the point light */}
+        <mesh>
+          <sphereGeometry args={[0.1]}/>
+        </mesh>
+      </pointLight>
+      <spotLight ref={spotRef} />
+    </>
+  )
+}
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [-1, 4, 2.5] }}>
-      <directionalLight position={[1, 1, 1]} />
+    <Canvas camera={{ position: [4, 4, 1.5] }}>
+      <Lights />
       <Polyhedron
         name="meshBasicMaterial"
         position={[-3, 1, 0]}
-        material={new THREE.MeshBasicMaterial()}
+        material={
+          new THREE.MeshBasicMaterial({ color: 'yellow', flatShading: true })
+        }
       />
       <Polyhedron
         name="meshNormalMaterial"
         position={[-1, 1, 0]}
-        material={new THREE.MeshNormalMaterial()}
+        material={new THREE.MeshNormalMaterial({ flatShading: true })}
       />
       <Polyhedron
         name="meshPhongMaterial"
         position={[1, 1, 0]}
-        material={new THREE.MeshPhongMaterial()}
+        material={
+          new THREE.MeshPhongMaterial({ color: 'lime', flatShading: true })
+        }
       />
       <Polyhedron
         name="meshStandardMaterial"
         position={[3, 1, 0]}
-        material={new THREE.MeshStandardMaterial()}
+        material={
+          new THREE.MeshStandardMaterial({
+            color: 0xff0033,
+            flatShading: true
+          })
+        }
       />
-      <OrbitControls target-y={1} />
+      <OrbitControls target={[2, 2, 0]} />
       <axesHelper args={[5]} />
       <gridHelper />
       <Stats />
