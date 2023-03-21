46b311c62feb47029999e14b812f4d04a212a1af
Alex <info.alessandrocoraglia@gmail.com>
2023-03-21 17:34:20 +0800

    Lesson 23
    Lesson about environment.
    The environment component is basically the background of the canvas and we can set panoramic images (like Potree had the clouds)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Stats, OrbitControls, Circle } from '@react-three/drei'
+import { Stats, OrbitControls, Circle, Environment } from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
@@ -9,14 +9,16 @@ export default function App() {
   return (
     <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
       <directionalLight position={[3.3, 1.0, 4.4]} intensity={3} castShadow />
+      <Environment files='./images/rustig_koppie_puresky_1k.hdr' background />
+      {/* <Environment files='./images/syferfontein_1d_clear_puresky_1k.hdr' background /> */}
       <primitive
         object={gltf.scene}
         position={[0, 1, 0]}
         children-0-castShadow
       />
-      <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
+      {/* <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
         <meshStandardMaterial />
-      </Circle>
+      </Circle> */}
       <OrbitControls target={[0, 1, 0]} />
       <axesHelper args={[5]} />
       <Stats />
