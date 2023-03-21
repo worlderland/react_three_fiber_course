74e77cef2f86817bde9e68db36548cd8b08c7e09
Alex <info.alessandrocoraglia@gmail.com>
2023-03-21 14:46:57 +0800

    Lesson 15
    Lesson about the gridHelper component. This also comes straight from three.js (like the axesHelper)

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ export default function App() {
       <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
       <OrbitControls />
       <axesHelper args={[5]} />
+      <gridHelper />
       <Stats />
     </Canvas>
   )
