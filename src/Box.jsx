f7b34118b4cb0437c1d5987e3f9ba9d563692f0b
Alex <info.alessandrocoraglia@gmail.com>
2023-03-21 11:28:35 +0800

    Lesson 5
    Lesson about useRef()

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,6 +1,10 @@
+import { useRef } from 'react'
+
 export default function Box(props) {
+    const ref = useRef()
+    console.log(ref);
   return (
-    <mesh {...props}>
+    <mesh {...props} ref={ref}>
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe />
     </mesh>
