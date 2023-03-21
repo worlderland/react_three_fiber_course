fe21e7690eac9e074f99fd8af7a6b209c82ecaae
Alex <info.alessandrocoraglia@gmail.com>
2023-03-21 15:43:04 +0800

    Lesson 17
    Lesson about materials.
    Listed and used just a few materials as example (basic, normal, phong and standard) but there are many others.
    Also moved the leva controls from App to the Polyhedron component to have all different controls for all different objects we created.

--- src/Polyhedron.jsx
+++ src/Polyhedron.jsx
@@ -1,21 +1,41 @@
-import { useRef, useState } from 'react'
+import { useRef } from 'react'
+import { useControls } from 'leva'
+import * as THREE from 'three'
+import { useFrame } from '@react-three/fiber'
 
-export default function Polyhedron({ polyhedron, color, ...props }) {
+export default function Polyhedron(props) {
   const ref = useRef()
-  const [count, setCount] = useState(2)
 
-  console.log(polyhedron[count].uuid)
+  useFrame((_, delta) => {
+    ref.current.rotation.x += 0.2 * delta
+    ref.current.rotation.y += 0.05 * delta
+  })
+
+  useControls(props.name, {
+    wireframe: {
+      value: false,
+      onChange: (v) => {
+        ref.current.material.wireframe = v
+      },
+    },
+    flatShading: {
+      value: true,
+      onChange: (v) => {
+        ref.current.material.flatShading = v
+        ref.current.material.needsUpdate = true
+      },
+    },
+    color: {
+      value: 'lime',
+      onChange: (v) => {
+        ref.current.material.color = new THREE.Color(v)
+      },
+    },
+  })
 
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      onPointerDown={() => {
-        setCount((count + 1) % 3)
-      }}
-      geometry={polyhedron[count]}
-    >
-      <meshBasicMaterial color={color} wireframe />
+    <mesh {...props} ref={ref}>
+      <icosahedronGeometry args={[1, 1]} />
     </mesh>
   )
 }
