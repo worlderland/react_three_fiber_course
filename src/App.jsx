c5d276bd721d0486a061a92aedae52dc68e38577
Alex <info.alessandrocoraglia@gmail.com>
2023-03-22 10:40:30 +0800

    Lesson 27
    Lesson about useGLTF (part 2)
    Seen how to load an array of models using the useGLTF hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,36 @@
-import {
-  Stats,
-  OrbitControls,
-  Environment,
-  ContactShadows
-} from '@react-three/drei'
-import { Canvas, useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { Leva, useControls } from 'leva'
+import { Canvas } from '@react-three/fiber'
+import { Stats, OrbitControls, Environment, useGLTF } from '@react-three/drei'
+import { useControls } from 'leva'
 
-function Model() {
-  const { scene } = useLoader(GLTFLoader, './models/scene.glb')
+const Models = [
+  { title: 'Hammer', url: './models/hammer.glb' },
+  { title: 'Drill', url: './models/drill.glb' },
+  { title: 'Tape Measure', url: './models/tapeMeasure.glb' }
+]
 
-  const {
-    x,
-    y,
-    z,
-    visible,
-    color,
-    metalness,
-    roughness,
-    clearcoat,
-    clearcoatRoughness,
-    transmission,
-    ior,
-    thickness
-  } = useControls('Suzanne', {
-    x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-    y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-    z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-    visible: true,
-    color: { value: '#ffbc85' },
-    metalness: { value: 0, min: 0, max: 1.0, step: 0.01 },
-    roughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
-    clearcoat: { value: 1, min: 0, max: 1.0, step: 0.01 },
-    clearcoatRoughness: { value: 0, min: 0, max: 1.0, step: 0.01 },
-    transmission: { value: 1.0, min: 0, max: 1.0, step: 0.01 },
-    ior: { value: 1.74, min: 1, max: 5, step: 0.01 },
-    thickness: { value: 3.12, min: 0, max: 5, step: 0.01 }
-  })
-
-  return (
-    <primitive
-      object={scene}
-      children-0-rotation={[x, y, z]}
-      children-0-visible={visible}
-      children-0-material-color={color}
-      children-0-material-metalness={metalness}
-      children-0-material-roughness={roughness}
-      children-0-material-clearcoat={clearcoat}
-      children-0-material-clearcoatRoughness={clearcoatRoughness}
-      children-0-material-transmission={transmission}
-      children-0-material-ior={ior}
-      children-0-material-thickness={thickness}
-    />
-  )
-}
-
-function Env() {
-  const { height, radius, scale } = useControls('Ground', {
-    height: { value: 10, min: 0, max: 100, step: 1 },
-    radius: { value: 115, min: 0, max: 1000, step: 1 },
-    scale: { value: 100, min: 0, max: 1000, step: 1 }
-  })
-  return (
-    <Environment
-      preset="sunset"
-      background
-      ground={{
-        height: height,
-        radius: radius,
-        scale: scale
-      }}
-    />
-  )
+function Model({ url }) {
+  const { scene } = useGLTF(url)
+  return <primitive object={scene} />
 }
 
 export default function App() {
+  const { title } = useControls({
+    title: {
+      options: Models.map(({ title }) => title)
+    }
+  })
+
   return (
     <>
-      <Canvas camera={{ position: [-8, 5, 8] }}>
-        <Env />
-        <Model />
-        <ContactShadows
-          scale={150}
-          position={[0.33, -0.33, 0.33]}
-          opacity={1.5}
-        />
-        <OrbitControls target={[0, 1, 0]} maxPolarAngle={Math.PI / 2} />
+      <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
+        <Environment files="./images/workshop_1k.hdr" background />
+        <group>
+          <Model url={Models[Models.findIndex((m) => m.title === title)].url} />
+        </group>
+        <OrbitControls  />
         <Stats />
       </Canvas>
-      <Leva collapsed />
+      <span id="info">The {title} is selected.</span>
     </>
   )
 }
