d694e00acd1d641cf384cdb0ae2ac98a6484e0eb
Alex <info.alessandrocoraglia@gmail.com>
2023-03-22 11:30:15 +0800

    Lesson 30
    Lesson about GLTFJSX (part 1).
    Here we used the command "npx gltfjsx ./public/models/shoe-draco.glb" to create the jsx code of the glb file.
    Interesting to see but don't really understand if it's relavant to what we need to do.

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,14 @@
-import { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
-import {
-  useGLTF,
-  OrbitControls,
-  Environment,
-  Stats,
-  Html
-} from '@react-three/drei'
-import { useControls } from 'leva'
-import Models from './models'
-
-function Model({ url }) {
-  const { scene } = useGLTF(url)
-  const [cache, setCache] = useState({})
-
-  if (!cache[url]) {
-    const annotations = []
-
-    scene.traverse((o) => {
-      if (o.userData.prop) {
-        annotations.push(
-          <Html
-            key={o.uuid}
-            position={[o.position.x, o.position.y, o.position.z]}
-            distanceFactor={0.25}>
-            <div className="annotation">{o.userData.prop}</div>
-          </Html>
-        )
-      }
-    })
-
-    console.log('Caching JSX for url ' + url)
-    setCache({
-      ...cache,
-      [url]: <primitive object={scene}>{annotations}</primitive>
-    })
-  }
-  return cache[url]
-}
+import { OrbitControls, Environment, ContactShadows } from '@react-three/drei'
+import { Model } from './Shoe'
 
 export default function App() {
-  const { model } = useControls({
-    model: {
-      value: 'hammer',
-      options: Object.keys(Models)
-    }
-  })
-
   return (
-    <>
-      <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
-        <Environment files="./images/workshop_1k.hdr" background />
-        <group>
-          <Model url={Models[model]} />
-        </group>
-        <OrbitControls autoRotate />
-        <Stats />
-      </Canvas>
-      <span id="info">
-        The {model.replace(/([A-Z])/g, ' $1').toLowerCase()} is selected.
-      </span>
-    </>
+    <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
+      <Environment preset="forest" />
+      <Model />
+      <ContactShadows position={[0, -0.8, 0]} color="#ffffff" />
+      <OrbitControls autoRotate />
+    </Canvas>
   )
 }
