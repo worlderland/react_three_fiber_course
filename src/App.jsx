5e03030627250e8e227704ddd2030e43a1b44191
Alex <info.alessandrocoraglia@gmail.com>
2023-03-22 13:14:02 +0800

    Lesson 32
    Lesson about lerping (making the camera moving based on mouse position)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Environment, ContactShadows } from '@react-three/drei'
-import { Model } from './Shoe'
+import { Canvas, useThree, useFrame } from '@react-three/fiber'
+import { Vector3 } from 'three'
+import { Stats, Environment, Center } from '@react-three/drei'
+import Button from './Button'
+
+const vec = new Vector3()
+
+function Rig() {
+  return useFrame(({ camera, mouse }) => {
+    vec.set(mouse.x * 2, mouse.y * 2, camera.position.z)
+    camera.position.lerp(vec, 0.025)
+    camera.lookAt(0, 0, 0)
+  })
+}
 
 export default function App() {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
-      <Environment preset="forest" />
-      <Model />
-      <ContactShadows position={[0, -0.8, 0]} color="#ffffff" />
-      <OrbitControls autoRotate />
+    <Canvas camera={{ position: [0, 0, 5] }}>
+      <Environment preset="forest" background />
+      <Center>
+        {[...Array(5).keys()].map((x) =>
+          [...Array(3).keys()].map((y) => (
+            <Button key={x + y * 5} position={[x * 2.5, y * 2.5, 0]} />
+          ))
+        )}
+      </Center>
+      <Rig />
+      <Stats />
     </Canvas>
   )
 }
