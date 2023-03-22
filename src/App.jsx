2d9c4f190a626abf7b1ad62024e25428017ac51c
Alex <info.alessandrocoraglia@gmail.com>
2023-03-22 08:57:35 +0800

    Lesson 24
    Lesson about advanced GLTF modelling in Blender.
    Not very cody...

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,43 @@
-import { Stats, OrbitControls, Circle, Environment } from '@react-three/drei'
+import { Stats, OrbitControls, Environment } from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { useControls } from 'leva'
+
+function Env() {
+  const { height, radius, scale } = useControls('Ground', {
+    height: { value: 10, min: 0, max: 100, step: 1 },
+    radius: { value: 115, min: 0, max: 1000, step: 1 },
+    scale: { value: 100, min: 0, max: 1000, step: 1 }
+  })
+  return (
+    <Environment
+      files="./images/rustig_koppie_puresky_1k.hdr"
+      background
+      ground={{
+        height: height,
+        radius: radius,
+        scale: scale
+      }}
+    />
+  )
+}
 
 export default function App() {
-  const monkey = useLoader(GLTFLoader, './models/monkey.glb')
-  const pipe = useLoader(GLTFLoader, './models/pipe/modular_industrial_pipes_01_1k.gltf')
-  console.log(monkey)
-  console.log(pipe)
+  const model = useLoader(GLTFLoader, './models/scene.glb')
+  //   const pipe = useLoader(GLTFLoader, './models/pipe/modular_industrial_pipes_01_1k.gltf')
+  console.log(model)
+  //   console.log(pipe)
 
   return (
     <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
-      <directionalLight position={[3.3, 1.0, 4.4]} intensity={3} castShadow />
-      <Environment files='./images/rustig_koppie_puresky_1k.hdr' background />
+        <Env />
       {/* <Environment files='./images/syferfontein_1d_clear_puresky_1k.hdr' background /> */}
-      <primitive
-        object={monkey.scene}
-        position={[0, 1, 0]}
-        children-0-castShadow
-      />
-      <primitive
+      <primitive object={model.scene} children-0-castShadow />
+      {/* <primitive
         object={pipe.scene}
         position={[0, 3, 0]}
         children-0-castShadow
-      />
+      /> */}
       {/* <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
         <meshStandardMaterial />
       </Circle> */}
